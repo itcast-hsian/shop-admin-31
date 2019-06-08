75f18687e82dd0dac00c3f83bf690a775d7bce2d
itcast-hsian <43436182+itcast-hsian@users.noreply.github.com>
2019-06-08 15:00:15 +0800

    登录页

---
+++ src/components/Mianbao.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="mianbao">
+        <el-breadcrumb separator="/">
+            <el-breadcrumb-item :to="{ path: '/' }">后台管理</el-breadcrumb-item>
+            <el-breadcrumb-item :to="{ path: '/' }">商品列表</el-breadcrumb-item>
+        </el-breadcrumb>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.mianbao{
+    padding-bottom: 10px;
+    border-bottom: 1px #ddd solid;
+}
+</style>
